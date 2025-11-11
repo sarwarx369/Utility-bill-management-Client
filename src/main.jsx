69ad4113b207e813c0a0b3ccc668e0f59a7da535
sarwarx369 <golamsarowarzahan@gmail.com>
2025-11-11 15:53:15 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,51 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import Root from "./components/Root.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from "./Pages/Home.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+
+        Component: Home,
+      },
+
+      // { path: "/register", Component: Register },
+      // { path: "/login", Component: Login },
+      // {
+      //   path: "/myprofile",
+      //   element: (
+      //     <ProtectedRoute>
+      //       <MyProfile />
+      //     </ProtectedRoute>
+      //   ),
+      // },
+      // {
+      //   path: "/toy/:id",
+      //   element: (
+      //     <ProtectedRoute>
+      //       <ToyDetails />
+      //     </ProtectedRoute>
+      //   ),
+      // },
+      // { path: "/dashboard", Component: Dashboard },
+      // { path: "*", Component: ErrorPage },
+      // {
+      //   path: "/forgot-password",
+      //   Component: ForgotPassword,
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
